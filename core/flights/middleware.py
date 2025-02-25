1535c20cc21bdfc0fc4e8986b8b276face520b7d
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-02-25 04:10:42 +0330

    updated plus plus 😠

--- core/flights/middleware.py
+++ core/flights/middleware.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
 
 class ErrorRedirectMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         if request.path == "/search/":
-            request.session["error_message"] = "یا مشکل از سروره یا اطلاعاتی که وارد کردی عزیزوم .دوباره امتخان کن:)"
-            return redirect("/")
+            request.session["error_message"] = "یا مشکل از سروره یا اطلاعاتی که وارد کردی مشکل داره.دوباره امتخان کن:)"
+            return redirect(reverse('flight:index'))
         return None
